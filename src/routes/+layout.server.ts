f0e3fb2eb23c1268cc10791bfcf47de6c3f6bd69
ranaldsgift <kevinjohncampbell@gmail.com>
2022-11-29 15:55:45 -0500

    Remove old vercel analytic injection

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,9 +1,7 @@
 import type { LayoutServerLoad } from './$types';
 import { getServerSession } from '@supabase/auth-helpers-sveltekit';
-import { inject } from '@vercel/analytics';
 
 export const load: LayoutServerLoad = async (event) => {
-  inject();
   return {
     session: await getServerSession(event)
   };
