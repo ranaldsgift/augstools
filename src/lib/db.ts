2f4513b29def7318d2a687dc0efa1a497832f12a
ranaldsgift <kevinjohncampbell@gmail.com>
2022-11-29 13:53:18 -0500

    Remove trace logging for env variables

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,6 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit'
 import { env } from '$env/dynamic/public'
 
-console.log(process.env.PUBLIC_SUPABASE_URL);
-
 let SUPABASE_URL = env.PUBLIC_SUPABASE_URL;
 let SUPABASE_KEY = env.PUBLIC_SUPABASE_ANON_KEY;
 
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -38,7 +38,6 @@ onMount(() => {
             drawerClose();
         } catch (error) {
             console.log(error);
-
         }
     }
 
